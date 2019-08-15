9e799bb84a740b474ebc349928cdc87117fa2078
Herman <williamshermanjr@gmail.com>
2019-08-15 10:55:41 -0500

    011 Create Users Table, Created schema

---
+++ server/database/schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE Ingredients;
+
+USE Ingredients;
+
+CREATE TABLE Users (
+  ID INT NOT NULL AUTO_INCREMENT,
+  Name varchar (50) NOT NULL,
+  Username varchar (50), NOT NULL,
+  Passwords varchar(50) NOT NULL,
+  Email varchar (50) NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE Foods (
+    ID INT NOT NULL AUTO_INCREMENT,
+    Item varchar (50) NOT NULL,
+    Expiration_Date varchar (50) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+SELECT Users.ID, Foods.Item FROM Users JOIN Foods ON Users.ID = Foods.Item;
