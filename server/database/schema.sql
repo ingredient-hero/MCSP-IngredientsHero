1a6543ad13bdeae08c521cd1b79f43f6fcd94a79
Herman <williamshermanjr@gmail.com>
2019-08-15 13:49:20 -0500

    011CreateUsersTable, added Expiration Table and joined Item and Users as well as Item and Expiration

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -14,8 +14,13 @@ CREATE TABLE Users (
 CREATE TABLE Foods (
     ID INT NOT NULL AUTO_INCREMENT,
     Item varchar (50) NOT NULL,
-    Expiration_Date varchar (50) NOT NULL,
     PRIMARY KEY (ID)
 );
 
+CREATE TABLE EXP (
+    ID INT NOT NULL AUTO_INCREMENT,
+    Expiration_Date DATE NOT NULL
+)
+
 SELECT Users.ID, Foods.Item FROM Users JOIN Foods ON Users.ID = Foods.Item;
+SELECT Foods.Item, EXP.Expiration_Date FROM Foods JOIN EXP ON Foods.Item = EXP.Expiration_Date;
