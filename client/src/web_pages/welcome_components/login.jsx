e614866536eb9e53862efd4b4de09b6a4988551e
Wesley Riley <wesleyj.riley22@gmail.com>
2019-08-16 10:36:37 -0500

    getting update from origin master

--- client/src/web_pages/welcome_components/login.jsx
+++ client/src/web_pages/welcome_components/login.jsx
@@ -1,1 +1,20 @@
- 
+import React from 'react';
+import ReactDom from'react-dom'
+import bootbox from 'bootbox';
+import bootstrap from 'bootstrap'
+
+const LoginModal = (props) => {
+
+    return(
+        <div>
+        
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#Login">
+            Login
+            </button>
+
+           
+        </div>
+    )
+}
+
+export default LoginModal;
