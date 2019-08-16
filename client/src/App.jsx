901771d53f9d0e24b168f467df6047024ea0746d
Shawnkeith <Shawnkeith1997@gmail.com>
2019-08-16 10:34:20 -0500

    Commiting to allow merge with master branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,9 +15,14 @@ export default class App extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
+        name: '',
+        userName: '',
+        password: '',
+        email: '',
         SignUp: '',
         Login: '',
       };
+        this.onChangeSighUp = this.onChangeSignUp.bind(this);
         this.onClickSignUp = this.onClickSignUp.bind(this);
         this.onClickLogin = this.onClickLogin.bind(this);
     }
@@ -26,10 +31,10 @@ export default class App extends React.Component {
     onClickSignUp(e){
       bootbox.confirm("<form id='infos' action=''>\
       Sign-Up <br/>\
-      Name:<input type='text' name='user_name' /><br/>\
-      Username:<input type='text' name='user_name' /><br/>\
-      Password:<input type='text' name='user_name' /><br/>\
-      Email:<input type='text' name='user_name' />\
+      Name:<input onChange={this.onChangeSighUp} class='name' type='text' name='name' >\
+      Username:<input onChange={this.onChangeSighUp} class='userName' type='text' name='userName' /><br/>\
+      Password:<input onChange={this.onChangeSighUp} class='password' type='text' name='password' />\
+      Email:<input onChange={this.onChangeSighUp} class='email' type='text' name='email' />\
       </form> ", function(result) {if(result)$('#infos').submit();
     })
   
@@ -37,6 +42,12 @@ export default class App extends React.Component {
     
   }
 
+  onChangeSignUp(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+
+  }
 
   onClickLogin(){
     console.log('clicked login in button')
@@ -47,7 +58,7 @@ export default class App extends React.Component {
     render() {
       return (
         <>
-          <Welcome onClickLogin={this.onClickLogin} onClickSignUp={this.onClickSignUp} SignUp={this.state.SignUp} Login={this.state.Login} />
+          <Welcome change={this.onChangeSignUp} onClickLogin={this.onClickLogin} onClickSignUp={this.onClickSignUp} name={this.state.name} username={this.state.userName} password={this.state.password} email={this.state.email} SignUp={this.state.SignUp} Login={this.state.Login} />
         </>
       );
     }
