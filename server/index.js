b953094266725d79fee7c2bb78d9cd7a2d40b2bb
Andrew Herrera <thinblueline2830@gmail.com>
2019-08-21 11:31:02 -0500

    Updated files to recent changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
+
+
+
+app.get('/welcome', (req, res) => {
   res.send('HELLO!')
 });
 app.listen(3000, () => console.log('Server running on port 3000'));
