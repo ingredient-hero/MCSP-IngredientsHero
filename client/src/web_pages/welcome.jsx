f599cb003b77058d3a8e5f659f1c3526a8327416
Shawnkeith <Shawnkeith1997@gmail.com>
2019-08-22 15:37:55 -0500

    Classnames added to components and homepage styling has been added.

--- client/src/web_pages/welcome.jsx
+++ client/src/web_pages/welcome.jsx
@@ -39,10 +39,12 @@ export default class Welcome extends React.Component {
     
         return(
            
-                <div>
+                <div id='Homepage'>
+                    <div className='pizza'></div>
                 <button style={{marginLeft: '1300px'}} disabled={false} type="button" onClick={this.props.onClickSignUp}>
                      Sign-Up
                 </button>
+                {/* <img className='backgroundImage' ></img> */}
                 <SignUp onSignupSubmitClick={this.props.onSignUpSubmitClick} hasClickedSignUp={this.props.hasClickedSignUp} 
                 change={this.props.onChangeLogin} onClickLogin={this.props.onClickLogin} onClickSignUp={this.props.onClickSignUp} 
                 name={this.props.name} username={this.props.userName} password={this.props.password} email={this.props.email} 
@@ -52,10 +54,10 @@ export default class Welcome extends React.Component {
                 grantUserAccess={this.props.grantUserAccess}/>
                 
                 <Logo/>
-                <h1 className='yolo' style={{fontSize:'60px', color:'blue', position: 'absolute', left: '25%', top: '35%'}}> Welcome to Ingredient Hero!</h1>
-                <p style={{position: 'absolute', right: '22%', left: '26%', top: '55%'}}> With the use of this app you will no longer have to worry about your perishable goods going to waste. We will help you to make the most of the items in your pantry, and offer you with simple recipes including the items available in your pantry! </p>
-                <h2 style={{fontSize:'60px', color:'blue', position:'absolute', left:'25%', top:'60%'}}>How to Use</h2>
-                <p style={{position: 'absolute', right: '22%', left: '26%', top: '80%'}}> To use this app start by either logging in or sign-up for a new account. Once you have signed in you will then be able to add items to your pantry. Based on these items that you add we will then supply you with a list of recipes that contains items closer to expiring.</p>
+                <h1 className='yolo1'> Welcome to Ingredient Hero!</h1>
+                <p className='p1'> With the use of this app you will no longer have to worry about your perishable goods going to waste. We will help you to make the most of the items in your pantry, and offer you with simple recipes including the items available in your pantry! </p>
+                <h2 className='yolo2' >How to Use</h2>
+                <p className='p2'> To use this app start by either logging in or sign-up for a new account. Once you have signed in you will then be able to add items to your pantry. Based on these items that you add we will then supply you with a list of recipes that contains items closer to expiring.</p>
             </div>
         );
     }
