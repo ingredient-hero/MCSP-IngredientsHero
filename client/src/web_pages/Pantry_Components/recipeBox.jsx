fc9aaa4060b9dd55ca89ac3455d8587f990e26fc
Collin Fairlie <collin.fairlie@gmail.com>
2019-08-23 19:44:43 -0500

    Made some changes to get the recipe api. Need to work out the .env and passing the API Key.

--- client/src/web_pages/Pantry_Components/recipeBox.jsx
+++ client/src/web_pages/Pantry_Components/recipeBox.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Recipe from './recipe.jsx';
 import axios from 'axios';
+// import dotenv from 'dotenv';
 
+const API_KEY = process.env._API_KEY;
 export default class RecipeBox extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
             count: 0,
+            URL: '',
         }
     }
 
@@ -14,9 +17,11 @@ export default class RecipeBox extends React.Component {
 
 
     componentDidMount () {
-        axios.get('https://api.spoonacular.com/recipes/random', `${process.env.SPOON_API_KEY}`)
+        console.log(process.env)
+        // console.log(process.env.REACT_APP_API_KEY)
+        axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}`)
         .then( res => {
-            console.log(res)
+            console.log(res.data)
         })
         .catch( err => {
             if (err) {
