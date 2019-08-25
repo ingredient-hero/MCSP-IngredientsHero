e6736ee2e5b48e9bd157dbb4dc61e2c754e0ab0a
Collin Fairlie <collin.fairlie@gmail.com>
2019-08-25 13:58:59 -0500

    Minor changes for css and coding out to avoid errors while working on things in pantry.

--- client/src/web_pages/Pantry_Components/notificationModal.jsx
+++ client/src/web_pages/Pantry_Components/notificationModal.jsx
@@ -1,49 +1,33 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-// import data from './dummy_data.js';
-import Modal from '../Welcome_Components/modal.jsx';
 
-const testingData = [{item: 'milk', exp: '2019-08-24'}, {item: 'bread', exp: '2019-08-24'}, {item: 'pancake', exp: '2019-08-24'}, {item: 'beans', exp: '2019-08-24'}]
+const testingData = [ {item: 'milk', exp: '2019-08-24'}, {item: 'bread', exp: '2019-08-24'}, 
+                      {item: 'pancake', exp: '2019-08-24'}, {item: 'beans', exp: '2019-08-24'}]
 
-export default class NotificationModal extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { 
-           
-      };
-     
-      
-    }
-  
-    
-  
-    render() {
-         let currentDate = new Date()
-      return (
-        <div className="ExpiredFood">
-          <button onClick={this.props.toggleModal} style={{marginLeft: "1300px", borderRadius: "50%"}}>
-            Notification
-          </button>
-  
-          <Modal show={!this.props.isOpen}
-            onClose={this.props.toggleModal}>
-            <h1>Food Is About to Expire</h1>
-            <form> 
-            {testingData.map((item, index) => {
-                // console.log(user)
-                // console.log(new Date(user.expiration) <= currentDate-3,'compare')
-                if(item.item && new Date(item.exp) >= currentDate - 3){
-                 return(
-                    <div key={index}>
-                      <li>{item.item} {item.exp}</li>
-                    </div>
-                        
-                    )
-                }
-            })}
-            </form>
-          </Modal>
-        </div>
-      );
-    }
+const NotificationModal = (props) => {
+    let currentDate = new Date()
+    return (
+      <div className="ExpiredFood">
+        <button onClick={props.toggleModal} style={{marginLeft: "1300px", borderRadius: "50%"}}>
+          Notification
+        </button>
+          <h1>Food Is About to Expire</h1>
+          <form> 
+          {testingData.map((item, index) => {
+              if(item.item && new Date(item.exp) >= currentDate - 3){
+                return(
+                  <div key={index}>
+                    <li>{item.item} {item.exp}</li>
+                  </div>
+                      
+                  )
+              }
+          })}
+          </form>
+      </div>
+    );
   }
+
+  export default NotificationModal;
+
+  // <Modal show={!this.props.isOpen}
+  // onClose={this.props.toggleModal}>
